f44c12b85988a806128d65fcf5ff7bdcc57638de
Van Thanh <136183859+vtmoazz@users.noreply.github.com>
2024-06-28 18:11:44 +0700

    Add files via upload

---
+++ check_prime.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(int n) {
+    if (n<= 1) return 0; 
+    int i;
+    int loop = sqrt(n);
+    for (i = 2; i <= loop; i++) {
+        if (n % i == 0) return 0; 
+    }
+    return 1; 
+}
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int found_prime = 0;
+    printf("\nOUTPUT\n");
+    for (int i = 0; i < n; i++) {
+        if (is_prime(arr[i])) {
+            printf("%d ", arr[i]);
+            found_prime = 1;
+        }
+    }
+    if (!found_prime) {
+        printf("No prime number exists");
+    }
+    printf("\n");
+
+    return 0;
+}
